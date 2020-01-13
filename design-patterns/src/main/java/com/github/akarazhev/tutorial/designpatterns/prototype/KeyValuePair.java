573149793c32d148c5edb4ecc1445a5674caf269
Andrey Karazhev <andrey.karazhev@specific-group.by>
2020-01-13 14:57:37 +0300

    Implemented the prototype pattern.

---
+++ design-patterns/src/main/java/com/github/akarazhev/tutorial/designpatterns/prototype/KeyValuePair.java
@@ -0,0 +1,85 @@
+package com.github.akarazhev.tutorial.designpatterns.prototype;
+
+import com.github.akarazhev.tutorial.designpatterns.builder.Validator;
+
+import java.util.Objects;
+
+/**
+ * Simple key-value util object.
+ */
+public final class KeyValuePair<T> implements Cloneable {
+    private final String key;
+    private final T value;
+
+    /**
+     * Constructs a pair model based on a key and value.
+     *
+     * @param key   a key.
+     * @param value a value.
+     */
+    public KeyValuePair(String key, T value) {
+        this.key = Validator.of(key).get();
+        this.value = Validator.of(value).get();
+    }
+
+    /**
+     * Returns a pair key pair.
+     *
+     * @return a key.
+     */
+    public String getKey() {
+        return key;
+    }
+
+    /**
+     * Returns a pair value.
+     *
+     * @return a value.
+     */
+    public T getValue() {
+        return value;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public KeyValuePair<T> clone() {
+        try {
+            return (KeyValuePair<T>) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        KeyValuePair<?> that = (KeyValuePair<?>) o;
+        return key.equals(that.key) &&
+                value.equals(that.value);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(key, value);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String toString() {
+        return "KeyValuePair{" +
+                "key='" + key + '\'' +
+                ", value=" + value +
+                '}';
+    }
+}
---
+++ design-patterns/src/test/java/com/github/akarazhev/tutorial/designpatterns/prototype/KeyValuePairTest.java
@@ -0,0 +1,22 @@
+package com.github.akarazhev.tutorial.designpatterns.prototype;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@DisplayName("KeyValuePair test")
+final class KeyValuePairTest {
+
+    @Test
+    @DisplayName("Compare a KeyValuePair")
+    void cloneKeyValuePair() {
+        final var pair = new KeyValuePair<>("key", "value");
+        // Check test results
+        assertEquals("key", pair.getKey());
+        assertEquals("value", pair.getValue());
+        final var clonedPair = pair.clone();
+        assertEquals("key", clonedPair.getKey());
+        assertEquals("value", clonedPair.getValue());
+    }
+}
