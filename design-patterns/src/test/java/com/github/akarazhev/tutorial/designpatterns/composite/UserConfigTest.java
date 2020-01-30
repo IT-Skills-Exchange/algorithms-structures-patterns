be23c7a1dd1e611126f7bc8e350b90985e87b21d
Andrey Karazhev <andrey.karazhev@specific-group.by>
2020-01-30 09:49:04 +0300

    Updated docs.

--- design-patterns/src/test/java/com/github/akarazhev/tutorial/designpatterns/composite/UserConfigTest.java
+++ design-patterns/src/test/java/com/github/akarazhev/tutorial/designpatterns/composite/UserConfigTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
- * Provides the сonfigs composite test.
+ * Provides the user сonfig composite test.
  */
 @DisplayName("User config test")
 final class UserConfigTest {
