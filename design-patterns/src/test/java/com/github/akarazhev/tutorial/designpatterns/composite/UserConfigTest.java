103a97ec18d20afc43469d5c91620f8e2ba5139c
Andrey Karazhev <andrey.karazhev@specific-group.by>
2020-01-31 11:46:28 +0300

    Updated tests.

--- design-patterns/src/test/java/com/github/akarazhev/tutorial/designpatterns/composite/UserConfigTest.java
+++ design-patterns/src/test/java/com/github/akarazhev/tutorial/designpatterns/composite/UserConfigTest.java
@@ -14,14 +14,14 @@ final class UserConfigTest {
     @Test
     @DisplayName("Get a user config id")
     void getUserConfigId() {
-        final UserConfig userConfig = new UserConfig(100, "Name");
+        final var userConfig = new UserConfig(100, "Name");
         assertEquals(100, userConfig.getId());
     }
 
     @Test
     @DisplayName("Get a user config name")
     void getUserConfigName() {
-        final UserConfig userConfig = new UserConfig(100, "Name");
+        final var userConfig = new UserConfig(100, "Name");
         assertEquals("Name", userConfig.getName());
     }
 }
