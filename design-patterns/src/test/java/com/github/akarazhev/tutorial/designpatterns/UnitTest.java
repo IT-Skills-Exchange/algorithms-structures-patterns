d112e1a8fe1477094375779359629582bcb858e5
Andrey Karazhev <andrey.karazhev@specific-group.by>
2020-02-19 10:24:30 +0300

    Updated the basic unit.

--- design-patterns/src/test/java/com/github/akarazhev/tutorial/designpatterns/UnitTest.java
+++ design-patterns/src/test/java/com/github/akarazhev/tutorial/designpatterns/UnitTest.java
@@ -7,6 +7,9 @@ import java.util.Collections;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+/**
+ * Provides basic tests
+ */
 public class UnitTest {
 
     protected Property getProperty() {
